d6057a6f46d40f5ae6c574653ca45b5853736534
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-25 16:45:54 -0500

    register user endpoint working

--- data/helpers/users-helpers.js
+++ data/helpers/users-helpers.js
@@ -1,7 +1,17 @@
 const db = require('../db-config');
 module.exports = {
     find,
+    findById,
+    insert,
 }
 function find() {
     return db('users')
+}
+
+function findById(id) {
+    return db('users').where({ id }).first()
+}
+
+function insert(user) {
+    return db('users').insert(user)
 }
--- routers/users-router.js
+++ routers/users-router.js
@@ -11,4 +11,30 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    const {id} = req.params;
+    try {
+        const user = await Users.findById(id)
+        res.status(200).json(user)
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({error: 'could not get user'})
+    }
+})
+
+router.post('/', async (req, res) => {
+    const user = req.body;
+    console.log(user)
+    try {
+        const [added] = await Users.insert(user)
+        const newUser = await Users.findById(added)
+        res.status(200).json(newUser)
+        console.log(added)
+        console.log(newUser)
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({error: 'could not add user'})
+    }
+})
+
 module.exports = router;
