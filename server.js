b151f73dc22649c3d70ded6e1f789823fc55b846
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-12-26 18:21:56 +0100

    adds pg and removes sessions

--- server.js
+++ server.js
@@ -13,25 +13,6 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(helmet())
-const sessionConfiguration = {
-    name: 'sandy',
-    secret: process.env.SECRET || 'sandy is a fat cat',
-    cookie: {
-        maxAge: 1000 * 60 * 60,
-        secure: false,
-        httpOnly: false,
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new KnexSessionStorage({
-        knex: knexConnection,
-        clearInterval: 1000 * 60 * 10, // delete expired sessions every 10 minutes
-        tablename: "user_sessions",
-        sidfieldname: "id",
-        createtable: true
-    })
-}
-server.use(session(sessionConfiguration))
 server.use('/api', apiRouter)
 
 
