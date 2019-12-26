d9d366ea73245a035872423d7bf0da4f309172a0
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-12-26 18:15:52 +0100

    adds migration seeds to production env

--- knexfile.js
+++ knexfile.js
@@ -45,8 +45,11 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    },
   }
 
 };
